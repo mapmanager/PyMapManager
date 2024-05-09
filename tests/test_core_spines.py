f697a0db54ef60a329eacf5b269e35df75efad73
Robert Cudmore <robert.cudmore@gmail.com>
2024-05-08 17:32:26 -0700

    merge with johnson 20240508

---
+++ tests/test_core_spines.py
@@ -0,0 +1,43 @@
+# from mapmanagercore import MapAnnotations, MultiImageLoader, MMapLoader
+from pymapmanager import stack
+
+from pymapmanager._logger import logger, setLogLevel
+
+def _tmpCudmoreTest():
+    zarrPath = '/Users/cudmore/Sites/MapManagerCore/sandbox/data/rr30a_s1.mmap'
+    
+    # _map = AnnotationsBaseMut(Loader())
+    # _map = MapAnnotations(MMapLoader(zarrPath).cached())
+    _stack = stack(zarrPath)
+
+    # update spine does not make sense before we add spine?
+    # _map.updateSpine(("spine_id", 0), {"z": 0})
+    # print('_map._points')
+    # print(_map._points)
+
+    pa = _stack.getPointAnnotations()
+    
+    beforeDf = pa.getDataFrame()
+    print(f'   beforeDf:{len(beforeDf)}')
+
+    logger.info('=== addSpine()')
+    _newSpineID = pa.addSpine(segmentID=0, x=100, y=100, z=10)
+
+    afterDf = pa.getDataFrame()
+    print(f'   afterDf:{len(afterDf)}')
+
+    logger.info('=== undo()')
+    pa.undo()
+
+    afterUndoDf = pa.getDataFrame()
+    print(f'   afterUndoDf:{len(afterUndoDf)}')
+
+    logger.info('=== moveSpine()')
+    pa.moveSpine(spineID=100, x=100, y=100, z=30)
+
+    afterMoveDf = pa.getDataFrame()
+    print(f"   afterMoveDf:{afterMoveDf.loc[100, ['x', 'y', 'z']]}")
+
+if __name__ == '__main__':
+    setLogLevel()
+    _tmpCudmoreTest()
