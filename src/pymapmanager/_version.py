f0ff0308690758d76d3a4fc7237dcaf97d25de44
Robert Cudmore <robert.cudmore@gmail.com>
2024-12-29 17:10:06 -0800

    tweaking github workflows for pytest

--- src/pymapmanager/_version.py
+++ src/pymapmanager/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.7.dev322+ge3d71a7.d20241230'
-__version_tuple__ = version_tuple = (0, 0, 7, 'dev322', 'ge3d71a7.d20241230')
+__version__ = version = '0.0.7.dev325+gfe7f229.d20241230'
+__version_tuple__ = version_tuple = (0, 0, 7, 'dev325', 'gfe7f229.d20241230')
