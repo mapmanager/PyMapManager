60facf1f9754f5b75b1f990e83dfd5d3c89e0e07
Robert Cudmore <robert.cudmore@gmail.com>
2025-01-14 15:48:28 -0800

    fixing pivot points

--- src/pymapmanager/interface2/stackWidgets/base/annotationPlotWidget2.py
+++ src/pymapmanager/interface2/stackWidgets/base/annotationPlotWidget2.py
@@ -1428,6 +1428,7 @@ class linePlotWidget(annotationPlotWidget):
 
         # TODO add option to show/hide
         # TODO add a z and mask if not in view (just like core points)
+        zPlusMinus = self._displayOptions["zPlusMinus"]
         pivotPointXs, pivotPointYs, pivotPointZs = self._annotations.getPivotPoint()
         self._pivotPoints.setData(pivotPointXs, pivotPointYs)
 
